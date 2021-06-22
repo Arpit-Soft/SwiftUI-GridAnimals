79cd9ca7eced457cd58cab6d483bb0d3a0f6ded2
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-22 23:37:36 +0530

    first commit

---
+++ SwiftUI-GridAnimals/SwiftUI_GridAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GridAnimalsApp.swift
+//  SwiftUI-GridAnimals
+//
+//  Created by Arpit Dixit on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GridAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
